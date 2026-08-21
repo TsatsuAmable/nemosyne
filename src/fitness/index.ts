4a890037b142ca40a7ff1716850debb456f32511
Tsatsu Amable <tsatsu.amable@gmail.com>
2026-08-21 17:01:36 +0100

    feat(fitness): export learned runtime adapter

--- src/fitness/index.ts
+++ src/fitness/index.ts
@@ -2,3 +2,4 @@ export * from './FitnessModelRegistry.ts';
 export * from './PairwiseLearning.ts';
 export * from './PromotionGate.ts';
 export * from './MonetaFeatureSnapshot.ts';
+export * from './LearnedFitnessRuntimeAdapter.ts';
