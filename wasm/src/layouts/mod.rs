a1db0bc6c1c82642babe0257dcbd668dc7cb1a2a
Tsatsu Amable <tsatsu.amable@gmail.com>
2026-08-23 14:05:50 +0100

    feat(moneta): register layout authority ABI exports

--- wasm/src/layouts/mod.rs
+++ wasm/src/layouts/mod.rs
@@ -4,6 +4,7 @@ pub mod radial_tree;
 pub mod time_ribbon;
 pub mod geo_surface;
 pub mod streamline;
+pub mod authority_abi;
 
 pub use grid::compute_grid_3d;
 pub use force_directed::compute_force_directed_3d;
