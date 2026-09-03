c586f7a192cd2fcb4a4fa7b5739278a8bef198bd
Tsatsu Amable <tsatsu.amable@gmail.com>
2026-09-03 23:31:54 +0100

    refactor: remove fake FlatBuffers export

--- src/data/serializers/index.ts
+++ src/data/serializers/index.ts
@@ -1,5 +1,3 @@
 export { datasetToArrowIPC, arrowIPCToDataset } from './ArrowSerializer.ts';
 
-export { datasetToFlatBuffer, flatBufferToDataset } from './FlatBuffersSerializer.ts';
-
 export { datasetToMessagePack, messagePackToDataset } from './MessagePackSerializer.ts';
