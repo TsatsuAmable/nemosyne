16b5ad5a05a08a6efcc3e64d685daa21bf9a878c
Tsatsu Amable <tsatsu.amable@gmail.com>
2026-09-05 08:11:14 +0100

    PT7: export runtime-neutral learning plane contracts

--- src/learning/index.ts
+++ src/learning/index.ts
@@ -1,2 +1,5 @@
 export * from './GestureEvaluationReport.ts';
 export * from './GestureTrainingSnapshotMaterializer.ts';
+export * from './ModelDeploymentManifest.ts';
+export * from './ReproducibleTrainingJob.ts';
+export * from './RuntimeModelRegistry.ts';
